bd5a73a797d6a1b90f67bf1ab65d213b63255340
theresiaherrich <124873369+theresiaherrich@users.noreply.github.com>
2023-08-24 14:20:20 +0700

    index template
    rozi

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,21 +1,25 @@
 @extends('layout')
 @section('container')
-    
+
 <div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="carousel">
     <div class="carousel-indicators">
       <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
       <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
       <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+      <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="3" aria-label="Slide 4"></button>
     </div>
     <div class="carousel-inner">
       <div class="carousel-item active">
-        <img src="img/bg.jpg" class="d-block w-100" alt="...">
+        <img src="img/carousel4.jpg" class="d-block w-100" alt="...">
       </div>
       <div class="carousel-item">
-        <img src="img/bg2.jpg" class="d-block w-100" alt="...">
+        <img src="img/carousel1.jpg" class="d-block w-100" alt="...">
       </div>
       <div class="carousel-item">
-        <img src="img/bg3.jpg" class="d-block w-100" alt="...">
+        <img src="img/carousel3.jpg" class="d-block w-100" alt="...">
+      </div>
+      <div class="carousel-item">
+        <img src="img/carousel2.jpg" class="d-block w-100" alt="...">
       </div>
     </div>
     <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
@@ -29,7 +33,7 @@
   </div>
 
   <div class="container py-5  ">
-   
+
         <div class="text-center title">
           <hr>
             <h1>Fresh Selections</h1>
@@ -40,24 +44,24 @@
   <div class="container ">
 
   <div class="row ">
-  
+
 
       <div class="col-lg-3 ">
       <div class="card  bg-dark">
         <img src="img/bg1.jpg" class="card-img" alt="...">
         <div class="card-img-overlay text-light">
           <h1 class="card-title">Card title</h1>
-         
+
         </div>
       </div>
       </div>
-   
+
         <div class="col-lg-3 ">
           <div class="card text-bg-dark">
             <img src="img/bg.jpg" class="card-img" alt="..." >
             <div class="card-img-overlay text-light">
               <h1 class="card-title">Card title</h1>
-              
+
             </div>
           </div>
           </div>
@@ -66,7 +70,7 @@
               <img src="img/bg3.jpg" class="card-img" alt="...">
               <div class="card-img-overlay text-light">
                 <h1 class="card-title">Card title</h1>
-                
+
               </div>
             </div>
             </div>
@@ -75,21 +79,21 @@
                 <img src="img/bg3.jpg" class="card-img" alt="...">
                 <div class="card-img-overlay text-light">
                   <h1 class="card-title">Card title</h1>
-                  
+
                 </div>
               </div>
               </div>
   </div>
-      
 
-     
-   
-    
+
+
+
+
   </div>
 
 
   <div class="container py-5">
-   
+
     <div class="text-center title">
       <hr>
         <h1>Tops Product</h1>
@@ -104,14 +108,14 @@
       <div class="card mb-3" style="max-width: 540px;">
         <div class="row g-0">
           <div class="col-md-4">
-            <img src="img/bg1.jpg" class="img-fluid rounded-start" alt="...">
+            <img src="img/Kangkung.webp" class="img-fluid rounded-start" alt="...">
           </div>
           <div class="col-md-4">
             <div class="card-body">
               <h5 class="card-title">Product</h5>
               <h6>Rp. 10.000 </h6>
 
-            
+
             </div>
           </div>
         </div>
@@ -128,7 +132,7 @@
               <h5 class="card-title">Product</h5>
               <h6>Rp. 10.000 </h6>
 
-            
+
             </div>
           </div>
         </div>
@@ -145,7 +149,7 @@
               <h5 class="card-title">Product</h5>
               <h6>Rp. 10.000 </h6>
 
-            
+
             </div>
           </div>
         </div>
@@ -162,7 +166,7 @@
               <h5 class="card-title">Product</h5>
               <h6>Rp. 10.000 </h6>
 
-            
+
             </div>
           </div>
         </div>
