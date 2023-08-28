40cb905bb4ac69ea03cf9b702a8837bc0cb840b7
theresiaherrich <124873369+theresiaherrich@users.noreply.github.com>
2023-08-28 08:57:38 +0700

    migration produk
    ojk sampe tabrakaan ges

--- app/Models/Produk.php
+++ app/Models/Produk.php
@@ -7,5 +7,30 @@ use Illuminate\Database\Eloquent\Model;
 
 class Produk extends Model
 {
-    use HasFactory;
+    protected $table = 'produk';
+    protected $fillable = [
+        'kategori_id',
+        'user_id',
+        'kode_produk',
+        'nama_produk',
+        'slug_produk',
+        'deskripsi_produk',
+        'foto',
+        'qty',
+        'satuan',
+        'harga',
+        'status',
+    ];
+
+    public function kategori() {
+        return $this->belongsTo('App\Kategori', 'kategori_id');
+    }
+
+    public function user() {
+        return $this->belongsTo('App\User', 'user_id');
+    }
+
+    public function images() {
+        return $this->hasMany('App\ProdukImage', 'produk_id');
+    }
 }
---
+++ app/Models/ProdukImage.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class ProdukImage extends Model
+{
+    protected $fillable = [
+        'produk_id',
+        'foto',
+    ];
+
+    public function produk() {
+        return $this->belongsTo('App\Produk','produk_id');
+    }
+}
+
