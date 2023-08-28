0acb1701ca30f5818674318956112a816d6bd4cf
M Fajar Tri <90810685+MFajarTrimulyo@users.noreply.github.com>
2023-08-28 09:44:55 +0700

    login migrate model

--- database/migrations/2023_08_24_063733_create_logins_table.php
+++ database/migrations/2023_08_24_063733_create_logins_table.php
@@ -13,6 +13,9 @@ return new class extends Migration
     {
         Schema::create('logins', function (Blueprint $table) {
             $table->id();
+            $table->string('username');
+            $table->string('email')->unique();
+            $table->string('pass');
             $table->timestamps();
         });
     }
