61c49f99325ee843b669dbd70a49a740dfee5be1
zeearchisa <142762598+zeearchisa@users.noreply.github.com>
2023-09-07 11:17:38 +0700

    contat

---
+++ resources/views/contact.blade.php
@@ -0,0 +1,66 @@
+@extends('layout')
+@section('container')
+
+<div class="container mt-5">
+<div class="text-center title">
+          
+            <h1>Contact Us</h1>
+            
+
+    </div>
+  <div class="row ">
+    
+      <div class="col-lg-6 card border-0 shadow mt-5 mb-md-0">
+        <div class="">
+          <h1 class="title font-weight-light text-white ">Contact Us</h1>
+          <form class="">
+            <div class="row">
+              <div class="col-lg-12">
+                <div class="form-group ">
+                  <input class="form-control text-white" type="text" placeholder="name">
+                </div>
+              </div>
+              <div class="col-lg-12">
+                <div class="form-group mt-2">
+                  <input class="form-control text-white" type="email" placeholder="email address">
+                </div>
+              </div>
+              <div class="col-lg-12">
+                <div class="form-group mt-2">
+                  <textarea class="form-control text-white" rows="3" placeholder="message"></textarea>
+                </div>
+              </div>
+              <div class="col-lg-12 d-flex align-items-center mt-2">
+                <button type="submit" class="btn btn-warning"><span> Submit</span></button>
+                <span class="ml-auto text-white align-self-center"><i class="icon-phone mr-2"></i>0857 4993 1120</span>
+              </div>
+              <div class="col-lg-12 mt-5">
+                <div class="row">
+                  <div class="col-lg-6">
+<h4><i class="bi bi-telephone"></i> +62-86389-1212-19</h4>
+                  </div>
+                  <div class="col-lg-6">
+<h4><i class="bi bi-instagram"></i> @pasar_segar</h4>
+                  </div>
+                  <div class="col-lg-6">
+<h4><i class="bi bi-envelope"></i> pasarsegar@gmail.com</h4>
+                  </div>
+                  <div class="col-lg-6">
+<h4><i class="bi bi-facebook"></i></i> pasar.segar</h4>
+                  </div>
+                </div>
+              </div>
+              
+            </div>
+          </form>
+        </div>
+      </div>
+    
+    <div class="col-lg-6 right-image p-r-0 mt-5">
+    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3951.606898289573!2d112.659064174643!3d-7.936059079036462!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2dd6299ed2c476ad%3A0xfba1cc2ab944bab!2sState%20Vocational%20High%20School%208%20Malang!5e0!3m2!1sen!2sid!4v1693809404996!5m2!1sen!2sid" width="600" height="400" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+    </div>
+  </div>
+</div>
+
+
+@endsection
