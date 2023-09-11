bbd739e23e07e7b0a3a3def896a7b5a605d3e6fb
M Fajar Tri <90810685+MFajarTrimulyo@users.noreply.github.com>
2023-09-11 12:28:22 +0700

    tambah produk & kategori sidebar admin

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -17,7 +17,9 @@ class RouteServiceProvider extends ServiceProvider
      *
      * @var string
      */
-    public const HOME = '/home';
+    public const ADMIN = '/admin';
+    public const HOME = '/';
+    public const LOGIN = '/login';
 
     /**
      * Define your route model bindings, pattern filters, and other route configuration.
