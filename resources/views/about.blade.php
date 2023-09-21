7e48d043032e5328832cd7b80b342d787803df4a
zeearchisa <142762598+zeearchisa@users.noreply.github.com>
2023-09-21 11:50:06 +0700

    about
    asdfg

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -8,27 +8,7 @@
         <h1 class="display-4">Pasar Segar</h1>
         <p class="lead text-muted mb-0">Pasar segar adalah tempat di mana Anda dapat menemukan berbagai jenis produk makanan dan bahan mentah yang masih dalam kondisi segar. Di pasar segar, Anda akan menemukan berbagai jenis buah, sayuran, daging, ikan, dan produk-produk segar lainnya. Produk-produk ini biasanya belum mengalami proses pengawetan atau pembekuan, sehingga memiliki masa simpan yang lebih pendek dibandingkan dengan produk olahan.</p>
       </div>
-      <div class="col-lg-6 d-none d-lg-block about"><img src="img/fruit.jpeg" alt="" class="img-fluid"></div>
-    </div>
-  </div>
-</div>
-
-<div class="bg-white py-5">
-  <div class="container py-5">
-    <div class="row align-items-center mb-5">
-      <div class="col-lg-6 order-2 order-lg-1">
-        <h1 class="font-weight-light">Frozen Food (Makanan Beku)</h1>
-        <p class="font-italic text-muted mb-4">Frozen food merujuk pada makanan yang telah diolah dan kemudian dibekukan untuk memperpanjang masa simpannya. Makanan beku dapat berupa berbagai jenis, seperti daging, ikan, sayuran, makanan siap saji, atau makanan penutup. Keuntungan utama dari makanan beku adalah kemampuan untuk menyimpannya dalam jangka waktu yang lebih lama, sehingga Anda dapat menggunakannya kapan saja sesuai kebutuhan.</p>
-      </div>
-      <div class="col-lg-5 px-5 mx-auto order-1 order-lg-2"><img src="https://bootstrapious.com/i/snippets/sn-about/img-1.jpg" alt="" class="img-fluid mb-4 mb-lg-0"></div>
-    </div>
-    
-    <div class="row align-items-center">
-      <div class="col-lg-5 px-5 mx-auto"><img src="https://bootstrapious.com/i/snippets/sn-about/img-2.jpg" alt="" class="img-fluid mb-4 mb-lg-0"></div>
-      <div class="col-lg-6">
-        <h1 class="font-weight-light">Bumbu</h1>
-        <p class="font-italic text-muted mb-4">Bumbu dapur adalah berbagai jenis bahan tambahan yang digunakan dalam memasak untuk memberikan rasa, aroma, dan cita rasa khusus pada hidangan. Bumbu dapur dapat berupa rempah-rempah, herba, saus, bahan pengental, atau bahan penyedap lainnya. Bumbu dapur sangat penting dalam memasak karena mereka dapat mengubah makanan biasa menjadi hidangan yang lezat dan beragam rasa. Setiap kultur memiliki bumbu-bumbu khas mereka sendiri yang digunakan dalam masakan tradisional.</p>
-      </div>
+      <div class="col-lg-6 d-none d-lg-block about"><img src="img/buah.png" alt="" class="img-fluid"></div>
     </div>
   </div>
 </div>
@@ -36,8 +16,8 @@
 <div class="bg-light py-5">
   <div class="container py-5">
     <div class="row mb-4">
-      <div class="col-lg-5">
-        <h2 class="display-4 font-weight-light">Our team</h2>
+      <div class="col-lg-12">
+        <h2 class="display-4 font-weight-light text-center">Our team</h2>
       </div>
     </div>
 
@@ -96,7 +76,7 @@
 
        <!-- Team item-->
        <div class="col-lg-4 col-sm-6 mb-5">
-        <div class="bg-white rounded shadow-sm py-5 px-4"><img src="https://bootstrapious.com/i/snippets/sn-about/avatar-3.png" alt="" width="100" class="img-fluid rounded-circle mb-3 img-thumbnail shadow-sm">
+        <div class="bg-white rounded shadow-sm py-5 px-4"><img src="img/dhana.jpg" alt="" width="100" class="img-fluid rounded-circle mb-3 img-thumbnail shadow-sm">
           <h5 class="mb-0">Dhana</h5><span class="small text-uppercase text-muted">Desain</span>
           <ul class="social mb-0 list-inline mt-3">
             <li class="list-inline-item"><a href="#" class="social-link"><i class="bi bi-telephone"></i> </a></li>
@@ -109,7 +89,7 @@
 
        <!-- Team item-->
        <div class="col-lg-4 col-sm-6 mb-5">
-        <div class="bg-white rounded shadow-sm py-5 px-4"><img src="https://bootstrapious.com/i/snippets/sn-about/avatar-3.png" alt="" width="100" class="img-fluid rounded-circle mb-3 img-thumbnail shadow-sm">
+        <div class="bg-white rounded shadow-sm py-5 px-4"><img src="img/hugo.jpg" alt="" width="100" class="img-fluid rounded-circle mb-3 img-thumbnail shadow-sm">
           <h5 class="mb-0">Hugo</h5><span class="small text-uppercase text-muted">Front-end</span>
           <ul class="social mb-0 list-inline mt-3">
             <li class="list-inline-item"><a href="#" class="social-link"><i class="bi bi-telephone"></i> </a></li>
