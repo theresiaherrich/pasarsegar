1559ba446ec6283a8781e83927875f6cb5b9cc78
HugoRGW <hugoraynor@gmail.com>
2023-10-05 13:11:15 +0700

    migration keranjang

---
+++ database/migrations/2023_10_05_035840_create_carts_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('carts', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id')->unsigned();
+            $table->string('no_invoice');
+            $table->string('status_cart');// ada 2 yaitu cart, checkout
+            $table->string('status_pembayaran');// ada 2 sudah dan belum
+            $table->string('status_pengiriman');// ada 2 yaitu belum dan sudah
+            $table->string('no_resi')->nullable();
+            $table->string('ekspedisi')->nullable();
+            $table->double('subtotal', 12, 2)->default(0);
+            $table->double('ongkir', 12, 2)->default(0);
+            $table->double('diskon', 12, 2)->default(0);
+            $table->double('total', 12, 2)->default(0);
+            $table->timestamps();
+
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('carts');
+    }
+};
---
+++ database/migrations/2023_10_05_035946_create_cart_detail_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('cart_detail', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('produk_id')->unsigned();
+            $table->unsignedBigInteger('cart_id')->unsigned();
+            $table->double('qty', 12, 2)->default(0);
+            $table->double('harga', 12, 2)->default(0);
+            $table->double('diskon', 12, 2)->default(0);
+            $table->double('subtotal', 12, 2)->default(0);
+            $table->timestamps();
+
+            $table->foreign('cart_id')->references('id')->on('carts')->onDelete('cascade');
+            $table->foreign('produk_id')->references('id')->on('produk')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('cart_detail');
+    }
+};
