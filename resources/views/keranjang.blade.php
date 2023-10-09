b82894c7d88d9ec17456c36d7e3400ac1db86341
M Fajar Tri <90810685+MFajarTrimulyo@users.noreply.github.com>
2023-10-09 10:03:18 +0700

    format keranjang

--- resources/views/keranjang.blade.php
+++ resources/views/keranjang.blade.php
@@ -1,118 +1,119 @@
 @extends('layout')
 @section('container')
-<div class="container py-5">
-  <div class="title mb-5 py-3" >
-  <h1 class="text-center">Keranjang</h1>
-</div>
-    <div class="row">
-      
-      <div class="col-lg-6 mx-5 ps-5">
+    <div class="container py-5">
+        <div class="title mb-5 py-3">
+            <h1 class="text-center">Keranjang</h1>
+        </div>
         <div class="row">
-        <div class="col-lg-12">
-        <div class="card mb-3 border-0 bg-white shadow" style="width: 450px;">
-  <div class="row ">
-    <div class="col-md-4">
-      <img src="img/k1.png" class="img-fluid rounded-start" alt="..." >
-    </div>
-    <div class="col-md-8">
-      <div class="card-body">
-        <div class="row">
-          <div class="col-lg-6">
-        <h1 class="card-title">Apel</h1>
-        <h5 class="text-danger">Rp. 10.000</h5>
-      </div>
-      <div class="col-lg-6 ">
-        <i class="bi bi-x-lg ms-5 px-5 text-danger py-5"></i>
-        
-      </div>
-      </div>
-     
-      </div>
-    </div>
-  </div>
-</div>
-</div>
-<div class="col-lg-12">
-        <div class="card mb-3 border-0 bg-white shadow" style="width: 450px;">
-  <div class="row ">
-    <div class="col-md-4">
-      <img src="img/k1.png" class="img-fluid rounded-start" alt="..." >
-    </div>
-    <div class="col-md-8">
-      <div class="card-body">
-        <div class="row">
-          <div class="col-lg-6">
-        <h1 class="card-title">Apel</h1>
-        <h5 class="text-danger">Rp. 10.000</h5>
-      </div>
-      <div class="col-lg-6 ">
-        <i class="bi bi-x-lg ms-5 px-5 text-danger py-5"></i>
-        
-      </div>
-      </div>
-     
-      </div>
-    </div>
-  </div>
-</div>
-</div>
-<div class="col-lg-12">
-        <div class="card mb-3 border-0 bg-white shadow" style="width: 450px;">
-  <div class="row ">
-    <div class="col-md-4">
-      <img src="img/k1.png" class="img-fluid rounded-start" alt="..." >
-    </div>
-    <div class="col-md-8">
-      <div class="card-body">
-        <div class="row">
-          <div class="col-lg-6">
-        <h1 class="card-title">Apel</h1>
-        <h5 class="text-danger">Rp. 10.000</h5>
-      </div>
-      <div class="col-lg-6 ">
-        <i class="bi bi-x-lg ms-5 px-5 text-danger py-5"></i>
-        
-      </div>
-      </div>
-     
-      </div>
-    </div>
-  </div>
-</div>
+
+            <div class="col-lg-6 mx-5 ps-5">
+                <div class="row">
+                    <div class="col-lg-12">
+                        <div class="card mb-3 border-0 bg-white shadow" style="width: 450px;">
+                            <div class="row ">
+                                <div class="col-md-4">
+                                    <img src="img/k1.png" class="img-fluid rounded-start" alt="...">
+                                </div>
+                                <div class="col-md-8">
+                                    <div class="card-body">
+                                        <div class="row">
+                                            <div class="col-lg-6">
+                                                <h1 class="card-title">Apel</h1>
+                                                <h5 class="text-danger">Rp. 10.000</h5>
+                                            </div>
+                                            <div class="col-lg-6 ">
+                                                <i class="bi bi-x-lg ms-5 px-5 text-danger py-5"></i>
+
+                                            </div>
+                                        </div>
+
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-12">
+                        <div class="card mb-3 border-0 bg-white shadow" style="width: 450px;">
+                            <div class="row ">
+                                <div class="col-md-4">
+                                    <img src="img/k1.png" class="img-fluid rounded-start" alt="...">
+                                </div>
+                                <div class="col-md-8">
+                                    <div class="card-body">
+                                        <div class="row">
+                                            <div class="col-lg-6">
+                                                <h1 class="card-title">Apel</h1>
+                                                <h5 class="text-danger">Rp. 10.000</h5>
+                                            </div>
+                                            <div class="col-lg-6 ">
+                                                <i class="bi bi-x-lg ms-5 px-5 text-danger py-5"></i>
+
+                                            </div>
+                                        </div>
+
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-12">
+                        <div class="card mb-3 border-0 bg-white shadow" style="width: 450px;">
+                            <div class="row ">
+                                <div class="col-md-4">
+                                    <img src="img/k1.png" class="img-fluid rounded-start" alt="...">
+                                </div>
+                                <div class="col-md-8">
+                                    <div class="card-body">
+                                        <div class="row">
+                                            <div class="col-lg-6">
+                                                <h1 class="card-title">Apel</h1>
+                                                <h5 class="text-danger">Rp. 10.000</h5>
+                                            </div>
+                                            <div class="col-lg-6 ">
+                                                <i class="bi bi-x-lg ms-5 px-5 text-danger py-5"></i>
+
+                                            </div>
+                                        </div>
+
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
 
 
 
-</div>
+                    </div>
 
-</div>
-    </div>
-    <div class="col-lg-4">
-      <div class="card shadow bg-white  ">
-    <h4 class="text-center mt-4 display-6">Chekout</h4>
-    <hr>
-   
-    <div class="row px-5 py-3">
-      <h3 class="mb-3">Ringkasan Belanja 
-      </h3>
-      <div class="col-lg-6">
-        <h5>Subtotal harga (1)</h5>
-      </div>
-      <div class="col-lg-1">
-        <h5 >:</h5>
-      </div>
-      <div class="col-lg-5">
-        <h5 class="text-danger">Rp. 100.000</h5>
-      </div>
-    </div>
-    <hr>
-   
-   <div class="p-3 mb-3">   
-    <center>
-     <button class="btn btn-success btn-lg " style="width: 100%">Chekout</button> </div>
-</center>
-      </div>
-    </div>
-  </div>
+                </div>
+            </div>
+            <div class="col-lg-4">
+                <div class="card shadow bg-white  ">
+                    <h4 class="text-center mt-4 display-6">Chekout</h4>
+                    <hr>
 
-</div>
+                    <div class="row px-5 py-3">
+                        <h3 class="mb-3">Ringkasan Belanja
+                        </h3>
+                        <div class="col-lg-6">
+                            <h5>Subtotal harga (1)</h5>
+                        </div>
+                        <div class="col-lg-1">
+                            <h5>:</h5>
+                        </div>
+                        <div class="col-lg-5">
+                            <h5 class="text-danger">Rp. 100.000</h5>
+                        </div>
+                    </div>
+                    <hr>
+
+                    <div class="p-3 mb-3">
+                        <center>
+                            <button class="btn btn-success btn-lg " style="width: 100%">Chekout</button>
+                    </div>
+                    </center>
+                </div>
+            </div>
+        </div>
+
+    </div>
 @endsection
