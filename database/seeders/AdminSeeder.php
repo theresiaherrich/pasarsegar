3f4521d9784c7eac715fbddaa2c34f8f193bb371
M Fajar Tri <90810685+MFajarTrimulyo@users.noreply.github.com>
2023-11-09 13:20:06 +0700

    kategori seeder

--- database/seeders/AdminSeeder.php
+++ database/seeders/AdminSeeder.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
+use Carbon\Carbon;
 
 class AdminSeeder extends Seeder
 {
@@ -18,7 +19,8 @@ class AdminSeeder extends Seeder
             'username' => 'Admin',
             'name' => 'Adminstrator',
             'email' => 'admin@admin.com',
-            'password' => Hash::make('adminaja')
+            'password' => Hash::make('adminaja'),
+            'created_at' => Carbon::now(),
         ]);
     }
 }
